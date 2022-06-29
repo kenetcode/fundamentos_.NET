25f459311f5ae267be19be4c157b4c5d29bb225e
kenetcode <k2ken2424@gmail.com>
2022-06-29 16:14:52 -0600

    Agregando la libreria y el codigo necesario para la funcionalidad de cambiar la edad de numeros a caracteres

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine("Por favor ingrese un nombre ");
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingrese un nombre ");
 var nombre = Console.ReadLine();
 Console.WriteLine("Por favor ingrese su cargo");
 var cargo = Console.ReadLine();
 Console.WriteLine("Por favor ingrese su edad");
-var edad = Console.ReadLine();
+var edad = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine($"Hola, Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años");
+Console.WriteLine($"Hola, Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
