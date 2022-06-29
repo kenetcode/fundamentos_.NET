287c8dac1711c2975d9d0c1f9eff371f5bae9bea
kenetcode <k2ken2424@gmail.com>
2022-06-29 15:57:27 -0600

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Por favor ingrese un nombre ");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad");
+var edad = Console.ReadLine();
+
+
+Console.WriteLine($"Hola, Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años");
